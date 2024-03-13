f54faf1a97923b4fa40f57217c2496e3024bf6c8
Leetram519 <82403725+Leetram519@users.noreply.github.com>
2024-03-13 13:15:52 +0100

    pouche
    Début de l'architecture MVC,
    avec redirection, refactorisation et header/footer softcodé

---
+++ web/index.php
@@ -0,0 +1,64 @@
+<?php
+
+if(empty($_GET['page'])){
+    $page = 'index';
+} else {
+    $url = explode("/", filter_var($_GET["page"], FILTER_SANITIZE_URL));
+    $page = $url[0];
+}
+
+try{
+    switch ($page){
+        case "index":
+            $page_description = "Page d'accueil";
+            $page_title = "Page d'accueil";
+            $page_content = "
+            <link href=\"./styles/index.css\" rel=\"stylesheet\" />
+            <div class=\"index-hero\">
+                <span class=\"index-text\">La recherche de stage simplifiée</span>
+                <h1 class=\"index-text1\">
+                    Où l'éducation rencontre l'expérience, pour un avenir professionnel brillant.
+                </h1>
+                <a href=\"login\" class=\"index-navlink button\"> S'authentifier </a>
+            </div>
+            ";
+            break;
+
+        case "login":
+            $page_description = "login";
+            $page_title = "login";
+            $page_content = "
+            <link href=\"./styles/login-page.css\" rel=\"stylesheet\" />
+            <div class=\"login-page-login-page\">
+                <h1 class=\"login-page-text\">
+                <span>Connection</span>
+                <br />
+                </h1>
+                <form class=\"login-page-form\" method=\"post\">
+                <input
+                    type=\"text\"
+                    placeholder=\"Nom d'utilisateur\"
+                    class=\"login-page-username input\"
+                />
+                <input
+                    type=\"password\"
+                    placeholder=\"Mot de passe\"
+                    class=\"login-page-password input\"
+                />
+                </form>
+                <a href=\"logged-in-landing.html\" class=\"login-page-navlink button\">
+                Se connecter
+                </a>
+            </div>
+            ";
+            break;
+
+        default:
+            throw new Exception("La page n'existe pas - ". $page);
+    }
+} catch(Exception $e){
+    $page_description = "Page 404";
+    $page_title = "Erreur";
+    $page_content = $e->getMessage();
+}
+require_once("layouts/base.php");
---
+++ web/layouts/base.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?= "Practicum | ".$page_title; ?></title>
+        <meta property="og:title" content="Practicum" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="utf-8" />
+        <meta name="description" content="<?= $page_description; ?>"/>
+    </head>
+
+    <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+        data-tag="font"
+    />
+    <link href='../styles/style.css' rel='stylesheet' />
+
+    <body>
+        <?php require_once("header.html"); ?>
+        <?= $page_content; ?>
+        <?php require_once("footer.html"); ?>
+    </body>
+</html>
