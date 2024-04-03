0468405476fcdf77168c3e1f9bd907449a362a50
Cyusa Adnan Junior <150007627+Mrj66@users.noreply.github.com>
2024-04-03 13:14:23 +0200

    mis a jour de pwa

--- web/main.js
+++ web/main.js
@@ -23,53 +23,10 @@ installButton.addEventListener('click', () => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
+    navigator.serviceWorker.register('/web/sw.js').then((registration) => {
       console.log('Service Worker registered:', registration);
     }).catch((error) => {
       console.error('Service Worker registration failed:', error);
     });
   });
 }
-
-const CACHE_NAME = 'pra';
-let offlineNotification = null;
-const URLS_TO_CACHE = [
-    'alert.css',
-    'style.css',
-    'logged-in-header.css',
-    'footer.css',
-    'header.css',
-    'search-bar.css',
-    'pagination.css',
-    'manifest.json',
-    'manifest.json'
-
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).then((networkResponse) => {
-        if (networkResponse.ok) {
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-          });
-        }
-        return networkResponse;
-      });
-    })
-  );
-});
-
-
